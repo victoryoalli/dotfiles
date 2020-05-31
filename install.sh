7c86d9d0653797ddd8339d9170d0a12b2f05bdf7
Victor Yoalli <victor@vientodigital.com>
2020-05-30 19:03:36 -0500

    Initial commit

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Make ZSH the default shell
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+echo 'Install oh-my-zsh'
+rm -rf $HOME/.oh-my-zsh
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Add global gitignore
+ln -s $HOME/.dotfiles/shell/.global-gitignore $HOME/.global-gitignore
+git config --global core.excludesfile $HOME/.global-gitignore
+
+# Symlink zsh prefs
+rm $HOME/.zshrc
+ln -s $HOME/.dotfiles/shell/.zshrc $HOME/.zshrc
+
+# Symlink vim prefs
+rm $HOME/.vimrc
+ln -s $HOME/.dotfiles/shell/.vimrc $HOME/.vimrc
+rm $HOME/.vim
+ln -s $HOME/.dotfiles/shell/.vim $HOME/.vim
+
+# Symlink yarn prefs
+#rm $HOME/.yarnrc
+#ln -s $HOME/.dotfiles/shell/.yarnrc $HOME/.yarnrc
+
+# Activate z
+cd ~/.dotfiles/shell
+
+echo 'Install composer'
+echo '----------------'
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar $HOME/bin/composer
+
+echo 'Install imagick'
+echo '---------------'
+pecl install imagick
+
+echo 'Install memcached'
+echo '-----------------'
+pecl install memcached
+
+echo 'Install xdebug'
+echo '--------------'
+pecl install xdebug
+
+echo 'Install redis'
+echo '-------------'
+pecl install redis
+
+#echo 'Install laravel envoy'
+#echo '---------------------'
+#composer global require "laravel/envoy=~1.0"
+
+#echo 'Install phpunit-watcher'
+#echo '-----------------------'
+#composer global require spatie/phpunit-watcher
+
+#echo 'Install mixed-content-scanner-cli'
+#echo '---------------------------------'
+#composer global require spatie/mixed-content-scanner-cli
+
+echo 'Install laravel valet'
+echo '---------------------'
+composer global require laravel/valet
+valet install
+
+#echo 'Install mysql'
+#echo '-------------'
+#brew install mysql@5.7
+#brew services start mysql@5.7
+
+echo 'Install ghostscript'
+echo '-------------------'
+brew install ghostscript
+
+echo 'Install mackup'
+echo '--------------'
+brew install mackup
+
+echo 'Install zsh-autosuggestions'
+echo '---------------------------'
+brew install zsh-autosuggestions
---
+++ shell/.zshrc
@@ -0,0 +1,40 @@
+# Path to your oh-my-zsh installation.
+export ZSH="/Users/victoryoalli/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+ZSH_THEME="robbyrussell"
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+#plugins=(git composer osx)
+plugins=(composer osx)
+
+source $ZSH/oh-my-zsh.sh
+
+# Enable autosuggestions
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+
+# Load the shell dotfiles, and then some:
+# * ~/.extra can be used for other settings you don’t want to commit.
+for file in ~/.dotfiles/shell/.{exports,aliases,functions}; do
+	[ -r "$file" ] && [ -f "$file" ] && source "$file"
+done
+unset file
+
+# Setup xdebug
+#export XDEBUG_CONFIG="idekey=VSCODE"
+
+# Alias hub to git
+#eval "$(hub alias -s)"
+
+# Extra PATH
+export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
